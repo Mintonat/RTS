09df6b14f048843a8fb26d52bf7b9c4515e156bc
DESKTOP-3DUGTF3\Drew <Mintonat@yahoo.com>
2018-01-14 21:20:53 -0500

    Host NetworkSetup

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+	public void MyStartHost(){
+		Debug.Log (Time.timeSinceLevelLoad + " Starting Host.");
+		StartHost ();	
+	}
+
+	public override void OnStartHost(){
+		Debug.Log (Time.timeSinceLevelLoad + " Host started.");
+	}
+
+	public override void OnStartClient (NetworkClient myClient){
+		Debug.Log (Time.timeSinceLevelLoad + " client start requiested.");
+		InvokeRepeating ("PrintDots", 0, 1f);
+	}
+
+	public override void OnClientConnect(NetworkConnection conn){
+		Debug.Log (Time.timeSinceLevelLoad + " client connected to IP:" + conn.address);
+		CancelInvoke ();
+	}
+
+	void PrintDots(){
+		Debug.Log (".");
+	}
+}
